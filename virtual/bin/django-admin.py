5718ca304450740f2e06fa39428308081d2aef58
Charity Mutoni <charitytoni0@gmail.com>
2021-04-29 22:38:40 +0200

    installed django

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/charity/Desktop/wecode/Python/Django/IP/Week2/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
