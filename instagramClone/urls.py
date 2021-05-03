862845a087d21e219cfcbb8bbbf64ee9f7773d0b
Charity Mutoni <charitytoni0@gmail.com>
2021-05-03 13:32:08 +0200

    installed crispy forms

--- instagramClone/urls.py
+++ instagramClone/urls.py
@@ -20,7 +20,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('instagram.urls'))
+    path('', include('insta.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    
